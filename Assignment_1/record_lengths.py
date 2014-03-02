295096162eb64c2938b50eb101c252fab26b18c7
centos <centos@localhost.localdomain>
2014-03-02 19:25:30 +0100

    A script to find the 3 longest sequences with  comments

--- Assignment_1/record_lengths.py
+++ Assignment_1/record_lengths.py
@@ -1,5 +1,18 @@
+#This script finds the three longest gene sequences in the fasta file NC_002695 
+#Import the SeqIO Biopython package
 from Bio import SeqIO
-filename = "NC_002695.fasta"
-for record in SeqIO.parse(filename, "fasta"):
-    if len(record) >6320:
- 	   print("Record " + record.id + ", length " + str(len(record.seq)))
+#Assign the NC_002965 as the input file
+input_file = "NC_002695.fasta"
+#Assign longest_genes as the output file
+output_file = "longest_genes.fasta"
+#Open the output file so the results can be written in
+output_handle = open(output_file, 'w') 
+#Put all the records into a list
+records = list(SeqIO.parse(input_file, 'fasta'))
+#Sort the records in the list by length
+records.sort(cmp = lambda x,y: cmp(len(y),len(x)))
+#Now write the longest three sequences into the output file
+SeqIO.write(records[0:3], output_handle, 'fasta')
+#Close the output file
+output_handle.close()
+
