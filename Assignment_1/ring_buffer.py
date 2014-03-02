e4466a81758386d46ae74483583cbf7459214127
centos <centos@localhost.localdomain>
2014-03-02 19:26:39 +0100

    a script to create a ring buffer with comments

--- Assignment_1/ring_buffer.py
+++ Assignment_1/ring_buffer.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
+#assign the infile to signal.txt
 infile="signal.txt"
+#read in the sequnces
 dataset=open(infile)
+#create a data array
 data=[]
+#read in the dataset into the data array: one floating point value per line
 for line in dataset.readlines():
 	line=line.strip()
 	for number in line.split():
 		data.append(float(number))
 dataset.close()
-buffersize=65
+#chose a buffersize; from trial and error it was found 60 was the optimum
+buffersize=60
+#create a buffer array
 buffer=[] 
 buffertotal=0 
 position=0 
 result=[]
+#fill in the buffer array, then calculate the moving average, followed by the running average
 while len(buffer)<buffersize:
 	buffer.append(data[position])
 	buffertotal=buffertotal+data[position]
@@ -24,7 +31,9 @@ for v in range(position, len(data)):
 	result.append(float(buffertotal)/buffersize)
 print result
 
+#open a new file called smoothed.txt 
 smooth=open('smoothed.txt', 'w')
+#write in the results from the ring buffer 
 for r in result:
         smooth.write("%s\n" % r)
 smooth.close()
