1e54f2ab09a8504726b1969ec1ffcacf73430930
Gillian Forsyth <g.w.forsyth@dundee.ac.uk>
2014-03-21 17:14:08 +0000

    chosen minus in templates since ita the untreated samples

--- project/DESeq2.R
+++ project/DESeq2.R
@@ -3,26 +3,27 @@ require(RCurl)
 require(biomaRt)
 require(stringr)
 
-#geneCounts<-read.delim("/homes/gwforsyth/BS32010/project/RNAseqCounts.txt",
-#                       head=T,sep="\t",skip=1, row.names=1)
+geneCounts<-read.delim("/homes/gwforsyth/BS32010/project/RNAseqCounts.txt",
+                       head=T,sep="\t",skip=1, row.names=1)
 
-geneCounts<-read.delim("/home/gwforsyth/Applied-Bioinformatics/project/RNAseqCounts.txt",
-                       head=T, sep="\t",skip=1, row.names=1)
+#geneCounts<-read.delim("/home/gwforsyth/Applied-Bioinformatics/project/RNAseqCounts.txt",
+#                       head=T, sep="\t",skip=1, row.names=1)
 
 nonZeroCounts<-geneCounts[rowSums(geneCounts[,6:28])>0,6:28]
 
-# l is plus, n is minus
+# samples are either plus or minus. 
 treatments <- as.factor(str_sub(colnames(nonZeroCounts),-9,-5))
 
 dds <- DESeqDataSetFromMatrix(as.matrix(nonZeroCounts),
                               as.data.frame(treatments),
                               design=~treatments)
 
-dds$treatments <- relevel(dds$treatments,"minus","_plus")
+dds$treatments <- relevel(dds$treatments,"minus")
 
 dds <- DESeq(dds)
 plotMA(dds, main="DESeq2")
 DEresults <- results(dds)
+write.csv(as.data.frame(DEresults), file="treatments_treated_results.csv")
 
 # get annotations
 mart <- useMart(biomart="ENSEMBL_MART_ENSEMBL", 
